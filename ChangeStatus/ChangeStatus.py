e17da099ad4f7990d110fca10e834b4572747450
Quinnella <97378595+Quinnella@users.noreply.github.com>
2022-02-13 14:44:24 +0800

    Update ChangeStatus.py

--- ChangeStatus/ChangeStatus.py
+++ ChangeStatus/ChangeStatus.py
@@ -19,7 +19,7 @@ class ChangeStatus(commands.Cog):
         await asyncio.sleep(10)
         await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.listening, name=f"{self.third}"))
         await asyncio.sleep(10)
-        await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Streaming(name=f"{self.fourth}", url="https://www.twitch.tv/somechannel"))
+        await self.bot.change_presence(activity=discord.Streaming(name=f"{self.fourth}", url="https://www.twitch.tv/somechannel"))
         await asyncio.sleep(10)
         await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.competing, name=f"{self.fifth}"))
         await asyncio.sleep(10)
