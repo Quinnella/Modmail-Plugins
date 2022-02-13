03859092dad8171b08b9610ca7419f87b3a7abfc
Quinnella <97378595+Quinnella@users.noreply.github.com>
2022-02-13 14:06:40 +0800

    Update ChangeStatus.py

--- ChangeStatus/ChangeStatus.py
+++ ChangeStatus/ChangeStatus.py
@@ -9,23 +9,28 @@ class ChangeStatus(commands.Cog):
         self.second = None
         self.third = None
         self.fourth = None
+        self.fifth = None
 
     @tasks.loop(seconds=10)
     async def start_the_status(self):
         await self.bot.change_presence(activity=discord.Game(name=f"{self.first}"))
+        await client.change_presence(status=discord.Status.dnd)
         await asyncio.sleep(10)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{self.second}"))
+        await client.change_presence(status=discord.Status.dnd)
         await asyncio.sleep(10)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{self.third}"))
+        await client.change_presence(status=discord.Status.dnd)
         await asyncio.sleep(10)
         await self.bot.change_presence(activity=discord.Streaming(name=f"{self.fourth}", url="https://www.twitch.tv/somechannel"))
         await asyncio.sleep(10)
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name=f"{self.fifth"))
 
     @commands.group(name="statusy", invoke_without_command=True)
     async def status_group(self, ctx):
         embed = discord.Embed(
             title="Change the Bot's Status!",
-            description=f"Change the Bot's Status to make it change every 10 Seconds!\n\nAvailable Commands:\n`{self.bot.prefix}statusy start`: Start the Status Changing Process\n`{self.bot.prefix}statusy one`: Set the first status of the bot!\n`{self.bot.prefix}statusy two`: Set the second status of the bot!\n`{self.bot.prefix}statusy three`: Set third first status of the bot!\n`{self.bot.prefix}statusy four`: Set the fourth status of the bot!",
+            description=f"Change the Bot's Status to make it change every 10 Seconds!\n\nAvailable Commands:\n`{self.bot.prefix}statusy start`: Start the Status Changing Process\n`{self.bot.prefix}statusy one`: Set the first status of the bot!\n`{self.bot.prefix}statusy two`: Set the second status of the bot!\n`{self.bot.prefix}statusy three`: Set third first status of the bot!\n`{self.bot.prefix}statusy four`: Set the fourth status of the bot!\n`{self.bot.prefix}statusy five`: Set the Fifth status of the bot!",
             color=self.bot.main_color
         )
         await ctx.send(embed=embed)
@@ -69,6 +74,14 @@ class ChangeStatus(commands.Cog):
         else:
             self.fourth = four
             await ctx.send(f"Set `{four}` as the fourth status!")
+
+    @status_group.command(name="five")
+    async def fifth_set(self, ctx, *, five):
+        if five == None:
+            await ctx.send("Please choose something to set!")
+        else:
+            self.fifth = five
+            await ctx.send(f"Set `{fifth}` as the fourth status!")
 
 
 def setup(bot):
